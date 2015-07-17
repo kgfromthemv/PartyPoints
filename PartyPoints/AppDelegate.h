8c34950407931fb58182299324decb4314f38d69
Kyle Grieder <kyle.grieder@gmail.com>
2015-07-16 20:09:03 -0600

    Initial Commit

---
+++ PartyPoints/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PartyPoints
+//
+//  Created by Kyle Grieder on 7/16/15.
+//  Copyright (c) 2015 Kyle Grieder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PartyPoints/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PartyPoints
+//
+//  Created by Kyle Grieder on 7/16/15.
+//  Copyright (c) 2015 Kyle Grieder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
