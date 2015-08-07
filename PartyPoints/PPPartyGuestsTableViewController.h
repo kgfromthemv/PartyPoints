6575ebea943035b74dbbd627656a584d2481e9c8
Kyle Grieder <kyle.grieder@gmail.com>
2015-08-06 18:23:07 -0600

    added up to date

---
+++ PartyPoints/PPPartyGuestsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPPartyGuestsTableViewController.h
+//  PartyPoints
+//
+//  Created by Kyle Grieder on 8/6/15.
+//  Copyright (c) 2015 Kyle Grieder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPPartyGuestsTableViewController : UITableViewController
+
+@end
